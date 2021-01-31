2ad6d5b2ea680dbe6fdbe8f5889df64563bb1036
akmal199601 <62462485+akmal199601@users.noreply.github.com>
2021-01-31 14:48:05 +0500

    sushnost

--- Program.cs
+++ Program.cs
@@ -1,48 +1,32 @@
 ﻿using System;
 
-namespace _26._01._20
+namespace sushnost
 {
-    class Program
+    class Person
+    {
+        public string FirtsName;
+        public string LastName;
+        public int age;     
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Имя: {FirtsName} \nФамилия:{LastName}  \nВозраст: {age}");
+        }
+    }
+              class Program
     {
         static void Main(string[] args)
         {
-    //     double a = Convert.ToDouble(Console.ReadLine());
-    //             double b = Convert.ToDouble(Console.ReadLine());
+            Person tom = new Person();
+            tom.GetInfo();      
 
-    //     var rectangle = new Rectangle (a,b);
-    //     Console.WriteLine($"Area: {rectangle.AreaCalculator()}");
-    //     Console.WriteLine($"Perimeter: {rectangle.PerimeterCalculator()}");
-    //     Console.ReadLine();
-            
-    //     }
-    //     public class Rectangle 
-    //     { 
-    //         double side1,side2;
-    //         public Rectangle (double side1, double side2)
-    //         {
-    //             this.side1 = side1;
-    //             this.side2 = side2;
-    //         }
-        
-    //     public double AreaCalculator()
-    //     {
-    //         return side1 * side2;
-    //     }
-    //     public double PerimeterCalculator()
-    //     {
-    //     return 2 * (side1 + side2);
-    //     }
-    //      public double Area
-    //     {
-    //     get { return AreaCalculator(); }
-    //     }
-    //     public double Perimeter
-    // {
-    //     get { return PerimeterCalculator(); }
-    // }
-
-
-     
-     } 
+            tom.FirtsName = "Tom";
+            tom.LastName = "Azizov";
+            tom.age = 34;
+            tom.GetInfo();  
+            Console.ReadKey();
+        }
     }
 }
+
+    
